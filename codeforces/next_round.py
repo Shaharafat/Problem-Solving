cc86e836624d55ba91b33ef05aa9a98b642cb3b8
shah_arafat <shah@co.design>
2022-03-15 11:41:18 +0600

    next round solved

---
+++ codeforces/next_round.py
@@ -0,0 +1,28 @@
+def calculate(first_line_input, second_line_input):
+
+    [n, k] = first_line_input.split(" ")
+    scores = second_line_input.split(" ")
+
+    n = int(n)
+    k = int(k)
+    next_round_participants_count = 0
+
+    scores = [int(x) for x in scores]
+
+    place_value = scores[k - 1]
+
+    for i in range(0, len(scores)):
+        if scores[i] > place_value and scores[i] > 0:
+            next_round_participants_count += 1
+        elif scores[i] == place_value and place_value != 0:
+            next_round_participants_count += 1
+
+    return next_round_participants_count
+
+
+if __name__ == "__main__":
+    first_line_input = input()
+    second_line_input = input()
+
+    result = calculate(first_line_input, second_line_input)
+    print(result)
