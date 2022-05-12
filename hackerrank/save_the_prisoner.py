882cf4e83339bc0258d0d5434f7a680223b8694b
shah_arafat <shah@co.design>
2022-05-12 17:21:44 +0600

    updated

--- hackerrank/save_the_prisoner.py
+++ hackerrank/save_the_prisoner.py
@@ -1,17 +1,9 @@
 def saveThePrisoner(n, m, s):
-  # first_covered = n - s + 1 
-  # remaining = n - first_covered
-  # print(first_covered,remaining)
-
-
-  # if m - first_covered :
-  #   return first_covered
-
-  # if n > m:
-  #   return m + s - 1
-  # else:
-  #   return m % n + s - 1
-  print((m % n )+ s - 1)
+  res = (s + m - 1) % n
+  if res == 0:
+    return n
+  else:
+    return res 
 
 saveThePrisoner(5,2,1)
 saveThePrisoner(3,7,3)
