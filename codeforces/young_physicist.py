ea451ec593c314b7770551f64e5ffdf6c645e40e
shah_arafat <shah@co.design>
2022-06-13 07:58:15 +0600

    updated

--- codeforces/young_physicist.py
+++ codeforces/young_physicist.py
@@ -1,14 +1,17 @@
 # Failed
 if __name__ == "__main__":
     n = int(input())
-    result = 0
+    x = 0
+    y = 0
+    z = 0
     for i in range(n):
         vector = list(map(lambda x: int(x), input().split(" ")))
 
-        for j in range(3):
-            result += vector[j]
+        x += vector[0]
+        y += vector[1]
+        z += vector[2]
 
-    if result == 0:
+    if x == 0 and y == 0 and z == 0:
         print("YES")
     else:
         print("NO")
